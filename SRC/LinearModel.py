0011578e249626c00ee92d21b1239ef302fb860c
nicolasbuitragob <nicolasbuitragobautista@gmail.com>
2022-04-23 23:17:30 -0500

    update logistic regression - change focus to dl

--- SRC/LinearModel.py
+++ SRC/LinearModel.py
@@ -5,31 +5,38 @@ import matplotlib.pyplot as plt
 class Classification():
     def __init__(self):
         self._iterations = 2000
-        self._learning_rate = 0.001
+        self._learning_rate = 0.01
         self.theta = None
+        self.bias = None
         self.cost = []
 
     def sigmoid(self,x):
         z = 1/(1 + np.exp(-x))
         return z
 
+    def plot_graph(self):
+        plt.plot(self.cost)
+        plt.show()
+    
     def fit(self,X,y):
         m,n = X.shape
-        self.theta = np.random.randn(n) 
-        X_with_bias = np.c_[np.ones(m),X] 
-        self.theta = np.insert(self.theta,0,0)
+        y = y.reshape((m,1))
+        self.theta = np.random.randn(n,1)
+        self.bias = 1
         for i in range(self._iterations):
-            h = np.dot(X_with_bias,self.theta)
+            h = np.dot(X,self.theta) + self.bias
             z = self.sigmoid(h)    
             J = (-1/m)*np.sum((y*np.log(z))+((1-y)*np.log(1-z)))
             error = z - y
-            grad = (1/m)*np.dot(X_with_bias.T,error)
+            grad = (1/m)*np.dot(X.T,error)
+            dbias = (1/m)*np.sum(error)
             self.theta =  self.theta - self._learning_rate*grad
+            self.bias = self.bias - self._learning_rate*dbias
             self.cost.append(J)
-        self.plot()
+            
+        self.plot_graph()
+        
 
     def predict(self, X_test):        
-        m_test = X_test.shape[0]
-        X_test_bias = np.c_[np.ones(m_test),X_test]
-        pred = np.dot(X_test_bias,self.theta)
+        pred = self.sigmoid(np.dot(X_test, self.theta) + self.bias)
         return (pred >= 0.5 )*1
